1a29c152d2125a4e9f985ca64b20fc1afb14fa21
mik-305 <mik-305@bk.ru>
2024-08-20 15:35:18 +0300

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,9 @@
+def divide(first, second):
+    if second == 0:
+        print('Результат деления на 0  - бесконечность! ')
+    else:
+        print('Результат деления:  ',first / second)
+    return
+
+
+# divide(10, 0.00001)
