db6d2e6d975943c49ba931ec7466e050211b2da0
mik-305 <mik-305@bk.ru>
2024-08-21 09:31:10 +0300

    Add files via upload

--- true_math.py
+++ true_math.py
@@ -1,6 +1,5 @@
+from math import inf
 def divide(first, second):
     if second == 0:
-        print('Результат деления на 0  - бесконечность! ')
-    else:
-        print('Результат деления:  ',first / second)
-    return
+        return inf
+    return first / second
